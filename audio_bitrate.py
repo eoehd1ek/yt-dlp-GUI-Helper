d33242c1be904ac9978f61fd78266181b59224f0
eoehd1ek <eoehd2ek@gmail.com>
2026-03-01 00:22:51 +0900

    remove: tkinter GUI 코드 삭제

--- audio_bitrate.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class AudioBitrate(Enum):
-    BEST = 0
-    GOOD = 2
-    NORMAL = 5
--- download_type.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class DownloadType(Enum):
-    NORMAL = 1
-    ONLY_MP3 = 2
-    HIGH_BITRATE_AUDIO_AND_VIDEO = 3
-    ONLY_MP4 = 4
